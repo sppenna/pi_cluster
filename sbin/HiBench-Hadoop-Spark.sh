12e95f4cb8275c354fd21d4ec3e3a89577ffcb69
Skyler Penna <leveetoo@gmail.com>
2019-03-28 22:17:04 -0700

    Update HiBench-Hadoop-Spark.sh

--- sbin/HiBench-Hadoop-Spark.sh
+++ sbin/HiBench-Hadoop-Spark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/pi/pi_bench/HiBench
-mvn -Phadoopbench -Psparkbench -Dspark=2.1 -Dscala=2.11 clean package
+mvn -f /home/pi/pi_cluster/HiBench/ -Phadoopbench -Psparkbench -Dspark=2.1 -Dscala=2.11 clean package
